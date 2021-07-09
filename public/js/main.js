1de1df57d8f09188bb1e0582fa99d2dabf7b0b7b
adayush17 <adayush17@gmail.com>
2021-07-09 10:57:35 +0530

    nodejs project first commit

---
+++ public/js/main.js
@@ -0,0 +1,96 @@
+const submitBtn = document.getElementById('submitBtn');
+const cityName = document.getElementById('cityName');
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
+const curDate = document.getElementById('day');
+const todayDate = document.getElementById('today_date');
+
+
+const getInfo = async(event) =>{
+    event.preventDefault();
+    let cityVal = cityName.value;
+    if(cityVal === ""){
+        city_name.innerText = `Please write the name before search`;
+        datahide.classList.add('data_hide');
+    }else{
+        try {
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=85edbaacb8aa8b7549952d74a737ad87`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const arrData = [data];
+
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            
+            const tempMode = arrData[0].weather[0].main;
+
+
+            // condition to check sunny or cloudy
+            if(tempMode == "Clear") {
+                temp_status.innerHTML =
+                   "<i class='fas fa-sun' style='color: #eccc68;'></i>";
+            }else if(tempMode == "Clouds"){
+                temp_status.innerHTML =
+                   "<i class='fas fa-cloud' style='color: #f1f2f6;'></i>";
+            }else if(tempMode == "Rain"){
+                temp_status.innerHTML =
+                   "<i class='fas fa-cloud-rain' style='color: #a4b0be;'></i>";
+            }else{
+                temp_status.innerHTML =
+                "<i class='fas fa-sun' style='color: #eccc68;'></i>";
+            }
+
+            datahide.classList.remove('data_hide');
+        
+        } catch {
+            city_name.innerText = `Please write the city name properly`;
+            datahide.classList.add('data_hide');
+        }
+        
+    }
+}
+
+ // for day time date 
+ const getCurrentDay = () => {
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thusday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let currentTime = new Date();
+    let day = weekday[currentTime.getDay()];
+    return day;
+};
+
+const getCurrentDate = () => {
+    var months = [
+        "JAN",
+        "FEB",
+        "MAR",
+        "APR",
+        "MAY",
+        "JUNE",
+        "JULY",
+        "AUG",
+        "SEPT",
+        "OCT",
+        "NOV",
+        "DEC",
+    ];
+    var now = new Date();
+    var month = months[now.getMonth() + 1];
+    var date = now.getDate();
+
+    return `${date} ${month}`;
+};
+
+curDate.innerHTML = getCurrentDay();
+todayDate.innerHTML = getCurrentDate();
+
+submitBtn.addEventListener('click', getInfo);
